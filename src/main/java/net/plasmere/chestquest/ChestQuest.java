bb360b788a29c1df1191fa236d986531ab05d082
zQuantumDev <tommy@skeffingtons.co,.uk>
2020-12-28 19:12:13 +0000

    v1.0.0

--- src/main/java/net/plasmere/chestquest/ChestQuest.java
+++ src/main/java/net/plasmere/chestquest/ChestQuest.java
@@ -1,4 +1,17 @@
 package net.plasmere.chestquest;
 
-public class ChestQuest {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChestQuest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
 }
