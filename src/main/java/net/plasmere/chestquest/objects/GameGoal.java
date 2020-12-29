6c4f88b3c58e4740a4b366d6be534f971af0cbb6
Nitrate <thesiberiantigercat@gmail.com>
2020-12-29 13:51:26 -0500

    Removed duped files.

--- src/main/java/net/plasmere/chestquest/objects/GameGoal.java
+++
@@ -1,9 +0,0 @@
-package net.plasmere.chestquest.objects;
-
-public enum GameGoal {
-    COMMON_REFILL, // Just Common Chests.
-    RARE_REFILL, // Just Rare Chests.
-    LEGENDARY_REFILL, // Just Legendary Chests.
-    SECONDARY, // Just Rare and Common Chests.
-    ALL // All Chests.
-}
