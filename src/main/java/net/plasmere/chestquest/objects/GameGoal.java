561da6463999669d1309310158790fef524c891f
Nitrate <thesiberiantigercat@gmail.com>
2020-12-29 13:22:49 -0500

    Fix clarification on GameGoal;

--- src/main/java/net/plasmere/chestquest/objects/GameGoal.java
+++ src/main/java/net/plasmere/chestquest/objects/GameGoal.java
@@ -1,9 +1,9 @@
 package net.plasmere.chestquest.objects;
 
 public enum GameGoal {
-    COMMON_REFILL,
-    RARE_REFILL,
-    LEGENDARY_REFILL,
-    SECONDARY,
-    ALL
+    COMMON_REFILL, // Just Common Chests.
+    RARE_REFILL, // Just Rare Chests.
+    LEGENDARY_REFILL, // Just Legendary Chests.
+    SECONDARY, // Just Rare and Common Chests.
+    ALL // All Chests.
 }
